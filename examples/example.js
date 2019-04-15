f79993bf6bb1ac55031ffb9ce15bb40c7247334d
Meet Dave <meet.dave@outlook.com>
2019-04-15 20:35:23 +0200

    Examples added

--- examples/example.js
+++ examples/example.js
@@ -1,9 +1,18 @@
-/* eslint-disable no-console */
-const defaultAwesomeFunction = require('../lib').default;
-const { awesomeFunction } = require('../lib');
+const nodeCCAvenue = require('../lib');
 
-const defaultVal = defaultAwesomeFunction('CCAvenue');
-const val = awesomeFunction();
+const ccav = new nodeCCAvenue.Configure({
+  working_key: 'workingkeygoeshere',
+  merchant_id: '123434'
+});
 
-console.log(defaultVal);
-console.log(val);
+// Refer https://dashboard.ccavenue.com/resources/integrationKit.do#response_parameters_doc for list of order parameters you can use here
+const orderParams = {
+  order_id: 8765432,
+  currency: 'INR',
+  amount: '100',
+  billing_name: 'Name of the customer',
+  // etc etc
+};
+
+const encryptedOrder = ccav.getEncryptedRequest(orderParams);
+console.log(encryptedOrder);
